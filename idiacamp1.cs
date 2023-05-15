f8968059f475d4d302af4efe2ce82ee2f8be02ee
bossdmt0312 <122552032+bossdmt0312@users.noreply.github.com>
2023-05-15 15:37:02 +0700

    Add files via upload

---
+++ idiacamp1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    Console.Write("Enter the maximum tank capacity: ");
+    double Vmax = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter the average water consumption during break time: ");
+    double tdrink = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter the amount of water that can be refilled in each round: ");
+    double tfill = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter the duration of break time: ");
+    int breaktime = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter the duration between rounds: ");
+    int roundtime = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter the total duration of the activity for the day: ");
+    int totaltime = Convert.ToInt32(Console.ReadLine());
+
+    int[] refillTime = new int[3];
+    refillTime[0] = breaktime;
+    refillTime[1] = breaktime + roundtime;
+    refillTime[2] = breaktime + (roundtime * 2);
+
+
+    double Vstart = Vmax / 2;
+
+
+    double V = Vstart;
+    bool enoughWater = true;
+    for (int i = 0; i < totaltime; i++) {
+      if (Array.IndexOf(refillTime, i) != -1) {
+        V += tfill;
+        if (V > Vmax) {
+          Console.WriteLine("Overflow Water");
+          break;
+        }
+      }
+      if (enoughWater) {
+        V -= tdrink;
+        if (V < 0) {
+          enoughWater = false;
+          Console.WriteLine("Not Enough Water");
+        }
+      }
+    }
+    if (enoughWater) {
+      Console.WriteLine("Enough Water, {0} left", V);
+    }
+  }
+}
---
+++ idiacamp2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    Console.Write("Enter the first balance amount: ");
+    double B1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter the second balance amount: ");
+    double B2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter the third balance amount: ");
+    double B3 = Convert.ToDouble(Console.ReadLine());
+
+    double L = 0;
+
+    Console.WriteLine("Enter payment amounts (enter negative or zero to stop): ");
+    while (true) {
+      double payment = Convert.ToDouble(Console.ReadLine());
+
+      if (payment <= 0) {
+        break;
+      }
+
+      if (B1 >= payment) {
+        B1 -= payment;
+      } else if (B2 >= payment) {
+        B2 -= payment;
+      } else if (B3 >= payment) {
+        B3 -= payment;
+      } else {
+        L += payment;
+      }
+    }
+
+    Console.WriteLine("Balance 1: {0}, Balance 2: {1}, Balance 3: {2}", B1, B2, B3);
+    Console.WriteLine("Left: {0}", L);
+  }
+}
